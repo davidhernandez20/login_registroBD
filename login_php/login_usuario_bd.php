b77c6ca65e8c9a25d7b91d614275c22cf29e849e
davidhernandez20 <luisdavidhernandez57@gmail.com>
2024-06-23 11:27:49 -0500

    se realiza completa la validacion del login

--- login_php/login_usuario_bd.php
+++ login_php/login_usuario_bd.php
@@ -6,10 +6,10 @@ $email = $_POST['email'];
 $contrasena =$_POST['contrasena'];
 
 
-$validar_login = mysqli_query($conexion, "SELECT FROM usuariosbd WHERE email ='$email' and contrasena  ='$contrasena' ");
+$validar_login = mysqli_query($conexion, "SELECT*FROM usuariosbd WHERE email ='$email' and contrasena  ='$contrasena' ");
 
 if(mysqli_num_rows($validar_login) >0){
-    header("location: ../pagina_en_blanco.php");
+    header("location: pagina_en_blanco.php");
     exit;
 }else{
     echo'
